18e93a59b6c0913c71097ad4c82bfd845c237e4b
mottox2 <y.mottox2@gmail.com>
2019-06-09 02:48:55 +0900

    Tweak scroll

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,21 @@ import React, { useState, useCallback, useRef } from 'react'
 import logo from './logo.svg'
 import './App.css'
 import { useSpring, animated } from 'react-spring'
+import { RemoveScroll } from 'react-remove-scroll'
 
 import GestureView from 'react-gesture-view'
 import { useGestureResponder, StateType, Callbacks, ResponderEvent } from 'react-gesture-responder'
 import { jsx, css, Global } from '@emotion/core'
 
-// const animationConfig = { mass: 0.8, tension: 185, friction: 24 }
-const animationConfig = { tension: 190, friction: 20, mass: 0.4 }
+const animationConfig = { mass: 0.8, tension: 185, friction: 24 }
+// const animationConfig = { tension: 190, friction: 20, mass: 0.4 }
 const App: React.FC = () => {
   const [index, setIndex] = useState()
   const [{ xy, ...other }, set] = useSpring(() => {
     return { xy: [0, 0], config: animationConfig }
   })
   const startVelocity = React.useRef<number | null>(null)
+  const [isScrollLocking, setScrollLocking] = useState(false)
 
   const shouldCloseOnRelease = (state: StateType) => {
     return false
@@ -51,24 +53,43 @@ const App: React.FC = () => {
   }
 
   const { bind } = useGestureResponder({
-    onStartShouldSet: () => false,
-    onMoveShouldSet: (state: StateType) => {
-      console.log(scrollableRef, state)
+    onStartShouldSet: (state: StateType) => {
       if (scrollableRef && scrollableRef.current) {
         if (state.direction[1]) {
-          return scrollableRef.current.scrollTop < 40 && state.direction[1] > 0
+          if (scrollableRef.current.scrollTop < 0) {
+            scrollableRef.current.scrollTop = 0
+          }
+          if (scrollableRef.current.scrollTop < 1 && state.direction[1] > 0) {
+            // setScrollLocking(true)
+            return true
+          }
         }
       }
       return false
     },
+    onMoveShouldSet: (state: StateType) => {
+      console.log(scrollableRef, state)
+      if (scrollableRef && scrollableRef.current) {
+        if (scrollableRef.current.scrollTop < 0) {
+          scrollableRef.current.scrollTop = 0
+        }
+        console.log(state.initialDirection, state.direction)
+        return (
+          scrollableRef.current.scrollTop < 1 &&
+          state.initialDirection[1] > 0 &&
+          state.initialDirection[1] == state.direction[1]
+        )
+      }
+      return false
+    },
     onMove: ({ delta, direction }) => {
-      console.log(delta, direction)
-      set({ xy: delta, config: animationConfig, immediate: true })
+      const [x, y] = delta
+      set({ xy: [x, Math.max(y, 0)], config: animationConfig, immediate: true })
     },
     onTerminate: onEnd,
     onRelease: (state: StateType) => {
       startVelocity.current = state.velocity
-      console.log(state)
+      setScrollLocking(false)
       onEnd(state)
     }
   })
@@ -77,74 +98,88 @@ const App: React.FC = () => {
       <Global
         styles={css`
           html {
-            background-color: #333;
+            background-color: rgba(0, 0, 0, 0.5);
             height: 100%;
           }
         `}
       />
-      <div
-        {...bind}
-        css={css`
-          position: fixed;
-          bottom: 0;
-          left: 0;
-          right: 0;
-          /* min-height: 300px; */
-        `}
-      >
-        <animated.div
+      <RemoveScroll>
+        <div
+          {...bind}
           css={css`
-            background-color: white;
-            &:after {
-              content: '';
-              position: fixed;
-              height: 100vh;
-              left: 0;
-              right: 0;
-              top: 100%;
-              background: white;
-            }
+            position: fixed;
+            bottom: 0;
+            left: 20px;
+            right: 20px;
+            /* min-height: 300px; */
           `}
-          style={{
-            // @ts-ignore
-            transform: xy.interpolate((x: number, y: number) => {
-              console.log(y)
-              return `translate3d(0, ${y}px, 0)`
-            })
-          }}
         >
-          <div
-            ref={scrollableRef}
+          <animated.div
             css={css`
-              height: 200px;
-              overflow-y: auto;
-              padding: 40px;
-              -webkit-overflow-scrolling: touch;
-
-              overflow-scrolling: touch;
+              background-color: white;
+              border-top-right-radius: 6px;
+              border-top-left-radius: 6px;
+              &:after {
+                content: '';
+                position: fixed;
+                height: 100vh;
+                left: 0;
+                right: 0;
+                top: 100%;
+                background: white;
+              }
             `}
+            style={{
+              // @ts-ignore
+              transform: xy.interpolate((x: number, y: number) => {
+                // console.log(y)
+                return `translate3d(0, ${y}px, 0)`
+              })
+            }}
           >
-            <h2>概要</h2>
-            <ul>
-              <li>
+            <div
+              ref={scrollableRef}
+              css={css`
+                height: 500px;
+                overflow-y: ${isScrollLocking ? 'hidden' : 'auto'};
+                padding: 20px;
+                -webkit-overflow-scrolling: touch;
+                /* -webkit-overflow-scrolling: auto; */
+                overflow-scrolling: touch;
+                p {
+                  line-height: 1.6;
+                }
+              `}
+            >
+              <h2>概要</h2>
+              <p>
                 SPAをリリースする際にVueやReactを単独でリリースしようとすると、表示までの速度や、ソーシャルシェア、SEOにおいて不利になりうる
-              </li>
-              <li>
+              </p>
+              <p>
                 Nuxt.js（のユニバーサルモード）を中心にSSRを利用する事例も増えてきたが、サーバー・キャッシュなどを考慮する必要があり面倒
-              </li>
-              <li>
+              </p>
+              <p>
                 SSG（静的サイトジェネレーター）でビルド時にSSRを行うPrerenderというアプローチを利用するとサーバーやキャッシュの考慮が不要になる
-              </li>
-              <li>
+              </p>
+              <p>
                 ただし、Prerenderはコンテンツの更新ごとにビルドの必要があり、反映までのタイムラグが存在する。
-              </li>
-              <li>
+              </p>
+              <p>
                 このデメリットを受け入れられれるのであれば、SSRよりSSGを利用するとメリットを享受できる。
-              </li>
-            </ul>
-          </div>
-        </animated.div>
-      </div>
+              </p>
+              <p>
+                SSG（静的サイトジェネレーター）でビルド時にSSRを行うPrerenderというアプローチを利用するとサーバーやキャッシュの考慮が不要になる
+              </p>
+              <p>
+                ただし、Prerenderはコンテンツの更新ごとにビルドの必要があり、反映までのタイムラグが存在する。
+              </p>
+              <p>
+                このデメリットを受け入れられれるのであれば、SSRよりSSGを利用するとメリットを享受できる。
+              </p>
+            </div>
+          </animated.div>
+        </div>
+      </RemoveScroll>
       {false && (
         <GestureView value={index} onRequestChange={i => setIndex(i)} enableMouse={true}>
           <header className="App-header">
@@ -166,6 +201,45 @@ const App: React.FC = () => {
           <div>page4</div>
         </GestureView>
       )}
+      <div>
+        <h2>概要</h2>
+        <p>
+          SPAをリリースする際にVueやReactを単独でリリースしようとすると、表示までの速度や、ソーシャルシェア、SEOにおいて不利になりうる
+        </p>
+        <p>
+          Nuxt.js（のユニバーサルモード）を中心にSSRを利用する事例も増えてきたが、サーバー・キャッシュなどを考慮する必要があり面倒
+        </p>
+        <p>
+          SSG（静的サイトジェネレーター）でビルド時にSSRを行うPrerenderというアプローチを利用するとサーバーやキャッシュの考慮が不要になる
+        </p>
+        <p>
+          ただし、Prerenderはコンテンツの更新ごとにビルドの必要があり、反映までのタイムラグが存在する。
+        </p>
+        <p>
+          このデメリットを受け入れられれるのであれば、SSRよりSSGを利用するとメリットを享受できる。
+        </p>
+        <p>
+          SSG（静的サイトジェネレーター）でビルド時にSSRを行うPrerenderというアプローチを利用するとサーバーやキャッシュの考慮が不要になる
+        </p>
+        <p>
+          ただし、Prerenderはコンテンツの更新ごとにビルドの必要があり、反映までのタイムラグが存在する。
+        </p>
+        <p>
+          このデメリットを受け入れられれるのであれば、SSRよりSSGを利用するとメリットを享受できる。
+        </p>
+        <p>
+          このデメリットを受け入れられれるのであれば、SSRよりSSGを利用するとメリットを享受できる。
+        </p>
+        <p>
+          SSG（静的サイトジェネレーター）でビルド時にSSRを行うPrerenderというアプローチを利用するとサーバーやキャッシュの考慮が不要になる
+        </p>
+        <p>
+          ただし、Prerenderはコンテンツの更新ごとにビルドの必要があり、反映までのタイムラグが存在する。
+        </p>
+        <p>
+          このデメリットを受け入れられれるのであれば、SSRよりSSGを利用するとメリットを享受できる。
+        </p>
+      </div>
     </div>
   )
 }
