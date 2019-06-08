e0b6612863f862af7c436aa2f820c9b04ecd6209
mottox2 <y.mottox2@gmail.com>
2019-06-08 21:28:33 +0900

    Add react-gesture-view

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
+
+import GestureView from 'react-gesture-view'
 
 const App: React.FC = () => {
+  const [index, setIndex] = useState(0)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <GestureView value={index} onRequestChange={i => setIndex(i)} enableMouse={true}>
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+        <div>page2</div>
+        <div>page3</div>
+        <div>page4</div>
+      </GestureView>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
